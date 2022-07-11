783331f7363ff669773b976ce2e08f0d3095284e
IsraelBoka <108950893+IsraelBoka@users.noreply.github.com>
2022-07-11 02:20:10 +0200

    problem: Getserversideprops avec email de session

--- pages/404.js
+++ pages/404.js
@@ -3,16 +3,12 @@ import Image from "next/image";
 export default function Custom404() {
   return (
     <div className="flex justify-center">
-            <div className="flex  mt-10 w-96 flex-col justify-center">
-        <Image 
-        src={'/astronaute.png'}
-        alt='test'
-        height={400}
-        width={500}
-        />
-        <div className="flex justify-center">404 erreur - Nous n&apos;avons pas pu trouver votre page.</div>
-    </div>
-
+      <div className="flex  mt-10 w-96 flex-col justify-center">
+        <Image src={"/astronaute.png"} alt="test" height={400} width={500} />
+        <div className="flex justify-center">
+          404 erreur - Nous n&apos;avons pas pu trouver votre page.
+        </div>
+      </div>
     </div>
   );
 }
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,34 +1,57 @@
 import React from 'react'
-import { GetServerSideProps } from 'next'
-import { PrismaClient, Session, User } from '@prisma/client'
+import { useRouter } from 'next/router';
+import { getSession } from "next-auth/react";
+import { useEffect, useState } from 'react';
 
-const prisma = new PrismaClient
-function profile({skyjson}:any) {
+
+function Profile({datastringify}:any) {
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+      getSession().then((session) => {
+        if (!session) {
+          router.replace('/');
+        }
+        else {
+            setIsLoading(false);
+          }
+      });
+    }, [router]);
+    if (isLoading) {
+        return <p>Loading...</p>;
+      }
+    
+  
   return (
     <div>
-        Have mercy on me !!
+        {datastringify}
         <br/>
         test
     </div>
   )
 }
-
 // You should use getServerSideProps when:
-// - Only if you need to pre-render a  page whose data must be fetched at request time
-
+// - Only if you need to pre-render a page whose data must be fetched at request time
+import { GetServerSideProps } from 'next'
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-    const sky = await prisma.user.findMany({
-        select:{
-            id: true,
-            email: true
-        }
-    })// your fetch function here
-    const skyjson = JSON.stringify(sky)
 
+    const sessionuser = await getSession(ctx)
+    console.log(sessionuser?.user?.email)
+    const emailuser = (sessionuser?.user?.email) 
+    const data  = await prisma.user.findUnique({
+        where:{
+            email: JSON.stringify(emailuser)
+        },
+    })
+
+    const datastringify = JSON.stringify(data)
     return {
         props: {
-            skyjson
+            datastringify
         }
     }
 }
-export default profile
+export default Profile
